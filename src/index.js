32d68a20bcf73fa9118ddb583e73ab44c01cc5e8
Nahuel <nahuelcarlos150@gmail.com>
2024-06-27 14:54:02 -0300

    Se crearon las solicitudes get

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ const express = require('express');
 app = express()
 libros = require("./database")
 
+app.use(express.json())
+
+app.get("/", (req,res) => {
+    res.json(libros)
+})
+
+app.get("/:id", (req,res) => {
+    idLibro = req.params.id
+    res.json(libros[idLibro-1])
+})
 
 app.listen(3000, () => {console.log("Server Running in port", 3000)})
