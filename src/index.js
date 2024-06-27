aff1b8f4aff613dacff2cc540e48fcb61608444d
Nahuel <nahuelcarlos150@gmail.com>
2024-06-27 16:20:46 -0300

    Se añadieron los extras

--- src/index.js
+++ src/index.js
@@ -6,47 +6,71 @@ app.use(express.json())
 
 const idGenerado = new Date().getTime()
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.json(libros)
 })
 
-app.get("/:id", (req,res) => {
+app.get("/:id", (req, res) => {
     id = req.params.id
     const index = libros.findIndex(libro => libro.id == id)
-    res.json(libros[index])
+    if (index == -1) {
+        res.send("El libro no existe")
+    }
+    else {
+        res.json(libros[index])
+    }
 })
 
-app.post("/",(req,res)=>{
+app.post("/", (req, res) => {
     const datajson = {
         id: idGenerado,
         title: req.body.title,
         author: req.body.author,
         year: req.body.year
     }
-    libros.push(datajson)
-    res.send("Libro Agregado")
+    const titleBook = req.body.title
+
+    const indexTitle = libros.findIndex(libro => libro.title == titleBook)
+    if (indexTitle == -1) {
+        libros.push(datajson)
+        res.send("Libro Agregado")
+    }
+    else {
+        res.send("El libro ya existe")
+    }
 })
 
-app.put("/:id", (req,res)=>{
+app.put("/:id", (req, res) => {
     id = req.params.id
     const datajson = {
-        id: idGenerado,
+        id: id,
         title: req.body.title,
         author: req.body.author,
         year: req.body.year
     }
     const index = libros.findIndex(libro => libro.id == id)
-    libros.splice(index,1,datajson)
-    res.send("Libro Actualizado")
+    if (index == -1) {
+        res.send("El libro que quiere actualizar no existe")
+    }
+    else {
+        libros.splice(index, 1, datajson)
+        res.send("Libro Actualizado")
+    }
 })
 
-app.delete("/:id", (req,res)=>{
+app.delete("/:id", (req, res) => {
     id = req.params.id
     const index = libros.findIndex(libro => libro.id == id)
-    libros.splice(index,1)
-    res.send("Libro Eliminado")
+    if (index == -1) {
+        res.send("El libro que quiere eliminar no existe")
+    }
+    else {
+        libros.splice(index, 1)
+        res.send("Libro Eliminado")
+    }
+    
 })
 
 
 
-app.listen(3000, () => {console.log("Server Running in port", 3000)})
+app.listen(3000, () => { console.log("Server Running in port", 3000) })
