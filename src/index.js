2f48fa10adfb4ec00ad15a14318101b38e96f82c
Nahuel <nahuelcarlos150@gmail.com>
2024-06-27 14:58:47 -0300

    Se hizo la solicitud POST

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ app.use(express.json())
 app.get("/", (req,res) => {
     res.json(libros)
 })
-
 app.get("/:id", (req,res) => {
     idLibro = req.params.id
     res.json(libros[idLibro-1])
+})
+
+app.post("/",(req,res)=>{
+    const {idLibro, nombre} = req.body
+    const datajson = {
+        idLibro: req.body.idLibro,
+        nombre: req.body.nombre
+    }
+    libros.push(datajson)
+    res.send("Libro Agregado")
 })
 
 app.listen(3000, () => {console.log("Server Running in port", 3000)})
